fd14790870cc9dc0ac4b078f081afd61c3b9f5fc
Bogong Yang <cmdroot9908@gmail.com>
2021-11-07 16:28:54 -0500

    Mofified structure for entries in RPC, added basic follower logic

--- testing/protocols.go
+++ testing/protocols.go
@@ -11,8 +11,14 @@ type AppendEntriesRPC struct {
 	LeaderId     int
 	PrevLogIndex int
 	PrevLogTerm  int
-	Entries      []string
+	Entries      []Log
 	CommitIndex  int
+}
+
+func (this *AppendEntriesRPC) PrintEntries() {
+	for i := range this.Entries {
+		fmt.Println(this.Entries[i].ToStr())
+	}
 }
 
 type AppendResp struct {
@@ -20,8 +26,34 @@ type AppendResp struct {
 	Success bool
 }
 
+type Responses struct {
+	Resps []AppendResp
+}
+
 type AppendReqs struct {
 	Rpcs []AppendEntriesRPC
+}
+
+type Log struct {
+	Term    int
+	Index   int
+	Command string
+}
+
+func (this *Log) ToStr() string {
+	return fmt.Sprintf("[Term: %d, Index: %d, command: %s]", this.Term, this.Index, this.Command)
+}
+
+func DeepCopyLogs(logs []Log) []Log {
+	result := make([]Log, len(logs))
+	for i := range logs {
+		result[i] = Log{}
+		result[i].Index = logs[i].Index
+		result[i].Term = logs[i].Term
+		result[i].Command = logs[i].Command
+	}
+
+	return result
 }
 
 // Unmarshal byte array into struct holding all the requests, propagate error
@@ -40,8 +72,14 @@ func ParseAppendReqFromFile(filename string) (AppendReqs, error) {
 func PrintAppendReqs(data *AppendReqs) {
 	for i := 0; i < len(data.Rpcs); i++ {
 		req := data.Rpcs[i]
-		fmt.Printf("term: %d, leaderId: %d, prevLogIndex: %d, prevLogTerm: %d, entries: ", req.Term, req.LeaderId, req.PrevLogIndex, req.PrevLogTerm)
-		fmt.Print(req.Entries)
-		fmt.Printf(", commitIndex: %d\n", req.CommitIndex)
+		fmt.Printf("term: %d, leaderId: %d, prevLogIndex: %d, prevLogTerm: %d, commitIndex: %d, entries:\n", req.Term, req.LeaderId, req.PrevLogIndex, req.PrevLogTerm, req.CommitIndex)
+		// fmt.Print(req.Entries)
+		req.PrintEntries()
+	}
+}
+
+func PrintResps(responses *Responses) {
+	for i := range responses.Resps {
+		fmt.Printf("{Term: %d, Success: %t}\n", responses.Resps[i].Term, responses.Resps[i].Success)
 	}
 }
--- testing/testFollower.go
+++ testing/testFollower.go
@@ -1,6 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+type Follower struct {
+	CurrentTerm int
+	Logs        []Log
+	Timeout     int
+}
+
+func (this *Follower) Init() {
+	this.CurrentTerm = 0
+	this.Logs = make([]Log, 0)
+	this.Timeout = 100
+}
+
+func (this *Follower) CheckPrev(index, term int) bool {
+	if index > len(this.Logs) {
+		return false
+	} else if index == 0 {
+		return len(this.Logs) == 0
+	}
+
+	log := this.Logs[index-1]
+	return log.Term == term
+	// if log.Term != term {
+	// 	return false
+	// }
+
+	// return true
+}
+
+func (this *Follower) HandleAppendEntriesRPC(requests *AppendReqs, responses *Responses) {
+	for _, rpc := range requests.Rpcs {
+
+		// validity check
+		if rpc.Term < this.CurrentTerm {
+			responses.Resps = append(responses.Resps, AppendResp{this.CurrentTerm, false})
+			continue
+		}
+
+		if rpc.Term > this.CurrentTerm {
+			this.CurrentTerm = rpc.Term
+		}
+
+		if len(rpc.Entries) == 0 {
+			// TODO: reset timer
+			continue
+		}
+
+		// return failure if log does not contain an entry at prevLogIndex whose
+		//  term matches prevLogTerm
+		if !this.CheckPrev(rpc.PrevLogIndex, rpc.PrevLogTerm) {
+			responses.Resps = append(responses.Resps, AppendResp{this.CurrentTerm, false})
+			continue
+		}
+
+		// check for conflicts
+		// need to sort the entries first
+		sort.SliceStable(rpc.Entries, func(i, j int) bool {
+			log_i := rpc.Entries[i]
+			log_j := rpc.Entries[j]
+
+			if log_i.Term < log_j.Term {
+				return true
+			} else if log_i.Term == log_j.Term {
+				return log_i.Index < log_j.Index
+			}
+
+			return false
+		})
+
+		conflict_index := -1
+		for i := rpc.PrevLogIndex; i < len(this.Logs); i++ {
+			if this.Logs[i].Term != rpc.Entries[0].Term || this.Logs[i].Index != rpc.Entries[0].Index || strings.Compare(rpc.Entries[0].Command, this.Logs[i].Command) != 0 {
+				conflict_index = i
+				break
+			}
+		}
+
+		if conflict_index != -1 {
+			this.Logs = this.Logs[:conflict_index]
+		}
+
+		// add all entries from RPC to follower's log
+		// deepcopy them to prevent some issues
+		// fmt.Println("Add new entry")
+		this.Logs = append(this.Logs, DeepCopyLogs(rpc.Entries)...)
+		responses.Resps = append(responses.Resps, AppendResp{this.CurrentTerm, true})
+	}
+}
 
 func main() {
 	data, err := ParseAppendReqFromFile("test_input/test_follower_same.json")
@@ -10,4 +105,28 @@ func main() {
 		fmt.Println("err")
 	}
 
+	responses := Responses{}
+	follower := Follower{}
+	follower.Init()
+	follower.HandleAppendEntriesRPC(&data, &responses)
+
+	// open an output file
+	out_file, out_file_err := os.OpenFile("follower_out.json", os.O_CREATE|os.O_WRONLY, 0777)
+	if out_file_err != nil {
+		panic(out_file_err)
+	}
+
+	PrintResps(&responses)
+
+	writter := bufio.NewWriter(out_file)
+	barr, json_err := json.MarshalIndent(responses, "", "    ")
+	// fmt.Print(barr)
+	// fmt.Println(len(responses.Resps))
+	if json_err != nil {
+		panic(json_err)
+	}
+	if _, write_err := writter.Write(barr); err != nil {
+		panic(write_err)
+	}
+	writter.Flush()
 }
